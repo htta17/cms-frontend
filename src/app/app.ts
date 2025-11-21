277b7c0ec56644d59d9c1d34d2d39670c7c0b4fc
Phuong Phan <phanphuong2503@gmail.com>
2025-11-21 09:28:28 -0600

    Test Build

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('cms-frontend');
+  protected readonly title = signal('Family CMS'); 
 }
